c44043d6a00552551d56ed522c0522f722e81c4d
Alexander Wachter <alexander@wachter.cloud>
2020-08-29 13:48:18 +0200

    wip

---
+++ drivers/can/can_common.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 Alexander Wachter
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef ZEPHYR_DRIVERS_CAN_COMMON_H_
+#define ZEPHYR_DRIVERS_CAN_COMMON_H_
+
+#include <drivers/can.h>
+#include <timeout_q.h>
+
+/* This context has to be at the beginning of each device data struct */
+struct can_tx_driver_ctx {
+	sys_slist_t send_list;
+	struct _timeout to;
+	struct can_send_ctx *next_to;
+	struct k_spinlock lock;
+};
+
+bool can_frame_prio_higher(const struct zcan_frame *frame1,
+			   const struct zcan_frame *frame2);
+
+#endif /*ZEPHYR_DRIVERS_CAN_COMMON_H_*/
